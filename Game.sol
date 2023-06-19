754b051b3a86c66975b8497da7cfdb3084456ae8
1KitCat1 <denischool119@gmail.com>
2023-06-19 16:14:06 +0300

    Updated for 2 players support only

--- Game.sol
+++ Game.sol
@@ -1,12 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity ^0.6.0;
 
 
-contract BoardGame {
+contract CastlediceGame {
+    uint8 constant public FIELD_HEIGHT = 10;
+    uint8 constant public FIELD_WIDTH = 10;    
+
     struct Room {
         address[] players;
-        mapping(address => bool) isPlayerActive;
         uint currentPlayerIndex;
-        uint[10][10] boardState;
+        mapping(uint8 => mapping(uint8 => BoardState)) boardState;
+        uint8 currentPlayerMoves;
     }
 
     mapping(uint => Room) public rooms;
@@ -17,22 +22,33 @@ contract BoardGame {
     }
 
     modifier onlyActivePlayer(uint roomId) {
-        require(rooms[roomId].isPlayerActive[msg.sender], "You are not a player of this room");
+        require(rooms[roomId].players[0] == msg.sender ||
+                rooms[roomId].players[1] == msg.sender , 
+                "You are not a player of this room");
+        _;
     }
 
-    function createRoom() external {
-        // todo
-    }
-
-    function joinRoom() external {
-        // todo
+    function createRoom(address[] calldata players) external {
+        countRooms++;
+        Room storage room = rooms[countRooms];
+        room.players = players;
+        for (uint8 i = 0; i < FIELD_HEIGHT; i++) {
+            for (uint8 j = 0; j < FIELD_WIDTH; j++) {
+                room.boardState[i][j] = BoardState.FREE; // 0 for FREE, 1 for RED, 2 for BLUE
+            }
+        }
+        room.boardState[0][0] = BoardState.BLUE;
+        room.boardState[9][9] = BoardState.RED;
     }
 
     function makeMove(uint roomId, uint row, uint col) external onlyActivePlayer(roomId){
-        Room storage room = rooms[roomId];
-
-        // todo: impement game logic;
-
-        room.currentPlayerIndex ^= 1;
+        
     }
 }
+
+enum BoardState {
+    FREE,
+    RED,
+    BLUE
+}
+// board state 
